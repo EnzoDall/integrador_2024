9f2d20cd2ba47611636db2db021a853e62fa36a9
EnzoDall <enzo.dallagnol@hotmail.com>
2024-07-08 15:37:55 -0300

    admin terminado

--- src/app/firebase.service.ts
+++ src/app/firebase.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore, DocumentChangeAction } from '@angular/fire/compat/firestore';
-import { Observable } from 'rxjs';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable, of } from 'rxjs';
+import { map, catchError } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,25 +12,81 @@ export class FirebaseService {
 
   constructor(private firestore: AngularFirestore) { }
 
-  addReserva(reserva: any) {
-    return this.firestore.collection('reservas').add(reserva);
+  addReserva(reserva: any): Promise<any> {
+    if (!this.isHoraMinimaAntecedenciaValida(reserva.data, reserva.horario)) {
+      return Promise.reject('Não é possível fazer uma reserva com menos de uma hora de antecedência.');
+    }
+    
+    return this.firestore.collection(this.collectionName).add(reserva);
   }
 
-  getReservasPorDataETipo(data: string, tipoMesa: string) {
-    return this.firestore.collection('reservas', ref => 
+  private isHoraMinimaAntecedenciaValida(data: string, horario: string): boolean {
+    const horaAtual = new Date();
+    const horaReserva = new Date(`${data}T${horario}`);
+
+    
+    horaAtual.setHours(horaAtual.getHours() + 1);
+
+    
+    return horaReserva > horaAtual;
+  }
+
+  getReservasPorDataETipo(data: string, tipoMesa: string): Observable<any> {
+    return this.firestore.collection(this.collectionName, ref => 
       ref.where('data', '==', data).where('tipoMesa', '==', tipoMesa)
-    ).get();
+    ).snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as any;
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      }),
+      catchError(error => {
+        console.error('Erro ao buscar reservas por data e tipo: ', error);
+        return of([]);
+      })
+    );
   }
 
-  getReservas(): Observable<DocumentChangeAction<unknown>[]> {
-    return this.firestore.collection(this.collectionName).snapshotChanges();
+  getReservas(): Observable<any[]> {
+    return this.firestore.collection(this.collectionName).snapshotChanges().pipe(
+      map(actions => {
+        return actions.map(a => {
+          const data = a.payload.doc.data() as any;
+          const id = a.payload.doc.id;
+          return { id, ...data };
+        });
+      }),
+      catchError(error => {
+        console.error('Erro ao buscar reservas: ', error);
+        return of([]);
+      })
+    );
   }
 
-  updateReserva(id: string, data: any) {
+  updateReserva(id: string, data: any): Promise<void> {
     return this.firestore.doc(`${this.collectionName}/${id}`).update(data);
   }
 
-  deleteReserva(id: string) {
+  deleteReserva(id: string): Promise<void> {
     return this.firestore.doc(`${this.collectionName}/${id}`).delete();
+  }
+
+  deleteExpiredReservas(): Promise<void> {
+    const now = new Date();
+    const currentDate = now.toISOString().split('T')[0];
+    const currentTime = `${now.getHours()}:${now.getMinutes()}`;
+
+    return this.firestore.collection(this.collectionName).ref.get().then(snapshot => {
+      const batch = this.firestore.firestore.batch();
+      snapshot.forEach(doc => {
+        const reserva = doc.data() as any;
+        if (reserva.data < currentDate || (reserva.data === currentDate && reserva.horario < currentTime)) {
+          batch.delete(doc.ref);
+        }
+      });
+      return batch.commit();
+    });
   }
 }
--- src/app/views/admin/admin.page.ts
+++ src/app/views/admin/admin.page.ts
@@ -3,6 +3,16 @@ import { FirebaseService } from '../../firebase.service';
 import { Observable, of } from 'rxjs';
 import { map, catchError } from 'rxjs/operators';
 
+interface Reserva {
+  id: string;
+  nome: string;
+  email: string;
+  numeroPessoas: number;
+  data: string;
+  horario: string;
+  tipoMesa: string;
+}
+
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.page.html',
@@ -10,33 +20,41 @@ import { map, catchError } from 'rxjs/operators';
 })
 export class AdminPage implements OnInit {
 
+  diasDisponiveis: string[] = [];
   horarios = ['18:30', '19:00', '19:30', '20:00', '20:30', '21:00', '21:30'];
-  reservasAgrupadas: { [key: string]: any[] } = {};
-  reservasVisiveis: { [key: string]: boolean } = {};
+  reservasAgrupadas: { [key: string]: { [key: string]: Reserva[] } } = {};
+  diaSelecionado: string | null = null;
   horarioSelecionado: string | null = null;
 
   constructor(private firebaseService: FirebaseService) {}
 
   ngOnInit() {
-    this.horarios.forEach(horario => {
-      this.reservasVisiveis[horario] = false;
-    });
-
     this.firebaseService.getReservas().pipe(
       map(actions => actions.map(a => {
-        const data = a.payload.doc.data() as any;
+        const data = a.payload.doc.data() as Reserva;
         const id = a.payload.doc.id;
-        return { id, ...data };
+        return { ...data, id }; 
       })),
       catchError(error => {
         console.error('Erro ao carregar reservas: ', error);
-        return of([]); // Retorna um array vazio em caso de erro
+        return of([] as Reserva[]); 
       })
     ).subscribe(reservas => {
-      this.horarios.forEach(horario => {
-        this.reservasAgrupadas[horario] = reservas.filter(reserva => reserva.horario === horario);
+      this.reservasAgrupadas = {};
+      this.diasDisponiveis = Array.from(new Set(reservas.map(reserva => reserva.data)));
+
+      this.diasDisponiveis.forEach(dia => {
+        this.reservasAgrupadas[dia] = this.horarios.reduce((acc: { [key: string]: Reserva[] }, horario: string) => {
+          acc[horario] = reservas.filter(reserva => reserva.data === dia && reserva.horario === horario);
+          return acc;
+        }, {});
       });
     });
+  }
+
+  selecionarDia(dia: string) {
+    this.diaSelecionado = dia;
+    this.horarioSelecionado = null;
   }
 
   toggleReservas(horario: string) {
